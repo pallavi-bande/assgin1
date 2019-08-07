2938209eea0d3a7b7b1ba6151cdad37669ec87ca
pallavi-bande <bandepallavib@gmail.com>
2019-08-07 16:28:41 +0530

    assgin1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //styles : [`
+ // h1{
+  //  color= aqua;
+ //  }
+ // `]
+
 })
 export class AppComponent {
   title = 'assignment1';
